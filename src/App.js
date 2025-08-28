fb9ea364281667637bb2d64ab1c7cae0ba695cbd
VuThienTan <21130170@st.hcmuaf.edu.vn>
2025-08-28 23:15:25 +0700

    final 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import ControlPanel from "./ControlPanel";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, HashRouter} from "react-router-dom";
 import RandomPage from "./Random";
 
 export default function App() {
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
-                <Route path="/" element={<ControlPanel />} />
-                <Route path="/random" element={<RandomPage />} />
+                <Route path="/" element={<ControlPanel/>}/>
+                <Route path="/random" element={<RandomPage/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
